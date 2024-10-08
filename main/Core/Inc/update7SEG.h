52bd517d21bd2717f02893a973476b1ad2a0ff56
LAPTOP-D1JJHVR7\Mr Duc Anh <anh.ngoducanh@hcmut.edu.vn>
2024-10-08 19:01:42 +0700

    Ex3

---
+++ main/Core/Inc/update7SEG.h
@@ -0,0 +1,15 @@
+/*
+ * update7SEG.h
+ *
+ *  Created on: Oct 8, 2024
+ *      Author: Mr Duc Anh
+ */
+
+#ifndef INC_UPDATE7SEG_H_
+#define INC_UPDATE7SEG_H_
+#include "main.h"
+#include "Component.h"
+
+void update7SEG ( int index );
+
+#endif /* INC_UPDATE7SEG_H_ */
---
+++ main/Core/Src/update7SEG.c
@@ -0,0 +1,43 @@
+/*
+ * update7SEG.c
+ *
+ *  Created on: Oct 8, 2024
+ *      Author: Mr Duc Anh
+ */
+
+
+#include "update7SEG.h"
+
+int led_buffer [4] = {1, 2, 3, 4};
+
+void update7SEG ( int index ){
+	switch (index){
+	case 0:
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 1);
+		break ;
+	case 1:
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 1);
+		break ;
+	case 2:
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 0);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 1);
+		break ;
+	case 3:
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 0);
+		break ;
+	default :
+		break ;
+	}
+	display7SEG(led_buffer[index]);
+}
